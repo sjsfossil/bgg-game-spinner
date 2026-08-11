41ee4eb27478c242bd3064d097e95ff487043dbc
sjsfossil <115507499+sjsfossil@users.noreply.github.com>
2026-08-11 14:12:13 -0500

    Add files via upload
    Version 10
    Logo changes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='howling-meeple-v9-2-reveal-learning';
+const CACHE='howling-meeple-v10';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./manifest.webmanifest'])))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))])));
 self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html')));return;}e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)));});
